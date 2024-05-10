17459433ea8a69047913315a30b6480dc18c3557
Ramashish <ramashishvishwakarma1@gmail.com>
2024-05-10 16:13:32 +0530

    changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World,hi there!");
 });
 app.listen(3000, () => {
   console.log(`server listening at http://localhost:3000}`);
